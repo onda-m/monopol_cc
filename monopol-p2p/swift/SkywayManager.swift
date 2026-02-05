7f971f1f38c1fb1b1fa00ea89da2f8cd272ea217
onda <onda-m@hotmail.co.jp>
2026-02-05 22:57:14 +0900

    start4

--- monopol-p2p/swift/SkywayManager.swift
+++ monopol-p2p/swift/SkywayManager.swift
@@ -576,7 +576,7 @@ class SkywayManager: NSObject, RoomDelegate, LocalRoomMemberDelegate, RoomPublic
     }
 
     @MainActor
-    fileprivate func leaveRoomIfNeeded(reason: String, setRoomClosed: Bool = true) async {
+    func leaveRoomIfNeeded(reason: String, setRoomClosed: Bool = true) async {
         print("[SkyMgr] leaveRoomIfNeeded start reason=\(reason)")
         logState("leaveRoomIfNeeded.begin")
         await detachRoomCallbacks(reason: reason)
